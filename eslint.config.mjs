fbcc8b3863e0d2544a9113a1aede5e9ce6200612
Santiago Martinez <smartinez9626@gmail.com>
2025-05-23 23:24:24 -0500

    chore: actualiza configuración de ESLint y TypeScript, desactiva regla de tipo de objeto vacío y añade rutas para módulos en tsconfig

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unsafe-assignment': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
       '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
       'linebreak-style': ['error', 'windows'],
     },
   },
