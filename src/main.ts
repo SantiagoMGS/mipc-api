52b5840146f6608f282384fcbc0d759df6b46951
Santiago Martinez <smartinez9626@gmail.com>
2025-05-23 20:23:43 -0500

    chore: inicializa estructura del proyecto

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('NestApplication');
+  const app = await NestFactory.create(AppModule);
+
+  logger.log(
+    `🚀 Server is running on port http://localhost:${process.env.PORT ?? 3000}`,
+  );
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
